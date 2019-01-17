59e34b4ff788c9611bddf728de927e58328db83f
Aleksey <aleksey@Mac-Aleksey.local>
2019-01-17 14:39:21 +0300

    Update Story Cell

--- MotoTestProject/Models/Story.swift
+++ MotoTestProject/Models/Story.swift
@@ -10,6 +10,40 @@ import Foundation
 
 struct Story: Decodable {
     let id: Int
+    
+    var storyData: StoryData?
+}
+
+struct StoryData: Decodable{
+    let id: Int
+    let authorName: String?
+    let commentCount: Int?
+    let score: Int?
+    let createdAt: Date?
+    let title: String?
+    let url: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case authorName = "by"
+        case commentCount = "descendants"
+        case score
+        case createdAt = "time"
+        case title
+        case url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.authorName = try? container.decode(String.self, forKey: .authorName)
+        self.commentCount = try? container.decode(Int.self, forKey: .commentCount)
+        self.score = try? container.decode(Int.self, forKey: .score)
+        self.createdAt = try? container.decode(Date.self, forKey: .createdAt)
+        self.title = try? container.decode(String.self, forKey: .title)
+        self.url = try? container.decode(String.self, forKey: .url)
+    }
+    
 }
 
 struct StoriesArray {
@@ -17,7 +51,7 @@ struct StoriesArray {
     let stories: [Story]
     
     init(ids: [Int]) {
-        stories = ids.map{Story(id: $0)}
+        stories = ids.map{Story.init(id: $0, storyData: nil)}
     }
     
 }
--- MotoTestProject/Views/Cells/StoryTableViewCell.swift
+++ MotoTestProject/Views/Cells/StoryTableViewCell.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class StoryTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var storyView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var commentsCountdateLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
     func configurate(story: Story){
-        titleLabel.text = String(story.id)
+        if let storyData = story.storyData{
+            activityIndicatorView.stopAnimating()
+            storyView.isHidden = false
+            titleLabel.text = storyData.title ?? "Empty title"
+            scoreLabel.text = String(storyData.score ?? 0)
+            commentsCountdateLabel.text = String(storyData.commentCount ?? 0)
+            authorLabel.text = storyData.authorName ?? ""
+            if let date = storyData.createdAt{
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateStyle = .short
+                dateLabel.text = dateFormatter.string(from: date)
+            }else{
+                dateLabel.text = ""
+            }
+        }else{
+            activityIndicatorView.startAnimating()
+            activityIndicatorView.isHidden = false
+            storyView.isHidden = true
+        }
     }
 
 }
